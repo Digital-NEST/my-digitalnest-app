4670709af8d367f17c6fcdc6fa3328f6b49c3f6c
bizznest <gilbert@digitalnest.org>
2023-01-26 11:06:36 -0800

    body content

--- content.md
+++ content.md
@@ -36,3 +36,35 @@ Primary: #BA2D8E #72C6A4
 ### Digital NEST Stories
 
 It’s time to offer an even playing ground for our young people and time to invest in a model proven to keep our youth home and in successful career-track jobs!
+
+### Get Involved
+
+Digital NEST doesn’t just connect youth to the internet and workforce training but to a broad network of local businesses, elected leaders, community organizations and neighbors who experience the ripple effects of opportunity and optimism seeded at the NEST.
+
+Card 1:
+
+- Your Donation Will Impact Lives
+- Every donation to Digital NEST connects more young people with opportunity, empowerment, and community that will propel them to career and life success.
+- Button
+  - Donate Now ->
+
+Card 2:
+
+- Become A Member Today
+- The NEST is a practice space – a place where it’s okay to be something new and try new things that you might not be good at… yet. The NEST exposes youth to various kinds of jobs and careers.
+
+### MEMBERS Programs
+
+Digital NEST’s Job Training and Career Development program help youth in high school to 24 years of age become competitive for entry-level-but-career-track jobs. NEST training is hands-on and project-based learning, plus you will build a portfolio of real, professional work
+
+## Sections
+
+- Logo + Navigation + Donate Button
+- Hero Video
+- NEST Vision
+- Stats + Digital NEST Stories
+- Get Involved (Cards)
+- Programs + Cards
+- BIZZNEST
+- Other Ways To Connect
+- Footer
--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,147 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
+import logo from './assets/nav-logo.png';
+import imgOne from './assets/nest-vision.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
+    <div>
+      <header class='header'>
+        <a href='#'>
+          <img class='logo' alt='Omnifood logo' src={logo} />
         </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
+
+        <nav class='main-nav'>
+          <ul class='main-nav-list'>
+            <li>
+              <a class='main-nav-link' href='#about'>
+                About
+              </a>
+            </li>
+            <li>
+              <a class='main-nav-link' href='#impact'>
+                Impact
+              </a>
+            </li>
+            <li>
+              <a class='main-nav-link' href='#Programs'>
+                Programs
+              </a>
+            </li>
+            <li>
+              <a class='main-nav-link' href='#pricing'>
+                Pricing
+              </a>
+            </li>
+            <li>
+              <a class='main-nav-link nav-cta' href='#cta'>
+                Donate
+              </a>
+            </li>
+          </ul>
+        </nav>
+
+        <button class='btn-mobile-nav'>
+          <ion-icon class='icon-mobile-nav' name='menu-outline'></ion-icon>
+          <ion-icon class='icon-mobile-nav' name='close-outline'></ion-icon>
         </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      </header>
+
+      <main>
+        <section className='section-hero'>
+          <div class='hero'>
+            <video className='hero-video' id='video-bg' controls>
+              <source
+                src='https://digitalnest.org/wp-content/uploads/2022/11/Untitled-3.mp4'
+                type='video/mp4'
+              />
+            </video>
+          </div>
+        </section>
+
+        <section class='section-how' id='how'>
+          <div class='container grid grid--2-cols grid--center-v'>
+            <div class='step-text-box'>
+              <p class='step-number'>Our Vision</p>
+              <h3 class='heading-primary'>NEST Vision</h3>
+              <p class='step-description'>
+                Latinx youth deserve the same access to resources to nurture
+                their talents, wisdom, and passions as their middle-class,
+                upper-class peers. Yet, these under-resourced communities remain
+                less likely to acquire career-building skills and graduate from
+                college or have robust networks to help them secure well-paying
+                jobs.
+              </p>
+
+              <p className='step-description-secondary'>
+                Digital NEST creates sustainable and scalable technology
+                learning centers for Latinx youth to provide the skills,
+                network, and resources to foster economic equality. We achieve
+                this by teaching our members the technical and soft skills to be
+                competitive in our digital world, providing mentors, and
+                broadening networks to gain sustainable employment.
+              </p>
+              <a class='main-nav-link nav-cta' href='#cta'>
+                Donate
+              </a>
+            </div>
+
+            <div className='step-img-box-one'>
+              <img src='' class='step-img' alt='' />
+            </div>
+
+            <div class='step-img-box'>
+              <img src='img/app/app-screen-2.png' class='step-img' alt='' />
+            </div>
+            <div class='step-text-box'>
+              <p class='step-number'>Impact</p>
+              <h3 class='heading-tertiary'>Digital NEST Stories</h3>
+              <p class='step-description'>
+                It’s time to offer an even playing ground for our young people
+                and time to invest in a model proven to keep our youth home and
+                in successful career-track jobs!
+              </p>
+            </div>
+
+            <div class='step-text-box'>
+              <p class='step-number'>Connect</p>
+              <h3 class='heading-tertiary'>Get Involved</h3>
+              <p class='step-description'>
+                Digital NEST doesn’t just connect youth to the internet and
+                workforce training but to a broad network of local businesses,
+                elected leaders, community organizations and neighbors who
+                experience the ripple effects of opportunity and optimism seeded
+                at the NEST.
+              </p>
+            </div>
+            <div class='step-img-box'>
+              <img src='img/app/app-screen-3.png' class='step-img' alt='' />
+            </div>
+          </div>
+        </section>
+
+        {/* <section class='section-featured'>
+          <div class='container'>
+            <h2 class='heading-featured-in'>As featured in</h2>
+            <div class='logos'>
+              <img src='img/logos/techcrunch.png' alt='Techcrunch logo' />
+              <img
+                src='img/logos/business-insider.png'
+                alt='Business Insider logo'
+              />
+              <img
+                src='img/logos/the-new-york-times.png'
+                alt='The New York Times logo'
+              />
+              <img src='img/logos/forbes.png' alt='Forbes logo' />
+              <img src='img/logos/usa-today.png' alt='USA Today logo' />
+            </div>
+          </div>
+        </section> */}
+      </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
